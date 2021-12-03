8c030e59788ade7b0a40f168f7fe3c677b634481
Patrick Boren <Pboren@lcs.on.ca>
2021-12-03 11:08:32 -0500

    Initial Commit

---
+++ TenoeratureConverterSwitchStatement/TenoeratureConverterSwitchStatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenoeratureConverterSwitchStatementApp.swift
+//  TenoeratureConverterSwitchStatement
+//
+//  Created by Patrick Boren on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct TenoeratureConverterSwitchStatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
